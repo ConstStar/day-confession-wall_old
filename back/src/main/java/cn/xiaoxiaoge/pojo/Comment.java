d50c3882c1b32341bbe0276a4abdfa3c10abbb2e
zhaoguoqingit <1164442003@qq.com>
2021-05-19 12:06:17 +0800

    缓存序列化

--- back/src/main/java/cn/xiaoxiaoge/pojo/Comment.java
+++ back/src/main/java/cn/xiaoxiaoge/pojo/Comment.java
@@ -1,19 +1,89 @@
 package cn.xiaoxiaoge.pojo;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
+import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Comment {
+public class Comment implements Serializable {
     private int id;
     private int pId;
 
     private String name;
     private Date createTime;
     private String content;
+
+    public Comment() {
+    }
+
+    public Comment(int id, int pId, String name, Date createTime, String content) {
+        this.id = id;
+        this.pId = pId;
+        this.name = name;
+        this.createTime = createTime;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getpId() {
+        return pId;
+    }
+
+    public void setpId(int pId) {
+        this.pId = pId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Comment comment = (Comment) o;
+        return id == comment.id && pId == comment.pId && Objects.equals(name, comment.name) && Objects.equals(createTime, comment.createTime) && Objects.equals(content, comment.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, pId, name, createTime, content);
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", pId=" + pId +
+                ", name='" + name + '\'' +
+                ", createTime=" + createTime +
+                ", content='" + content + '\'' +
+                '}';
+    }
 }
--- back/src/main/java/cn/xiaoxiaoge/pojo/Post.java
+++ back/src/main/java/cn/xiaoxiaoge/pojo/Post.java
@@ -1,15 +1,10 @@
 package cn.xiaoxiaoge.pojo;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
+import java.io.Serializable;
 import java.util.Date;
+import java.util.Objects;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Post {
+public class Post implements Serializable {
     private int id;
 
     private String sender;
@@ -23,4 +18,119 @@ public class Post {
 
     private int thumbsUp;       //点赞数量
     private int commentCount;   //评论数量
+
+    public Post() {
+    }
+
+    public Post(int id, String sender, int senderSex, String recipient, int recipientSex, Date createTime, String content, int thumbsUp, int commentCount) {
+        this.id = id;
+        this.sender = sender;
+        this.senderSex = senderSex;
+        this.recipient = recipient;
+        this.recipientSex = recipientSex;
+        this.createTime = createTime;
+        this.content = content;
+        this.thumbsUp = thumbsUp;
+        this.commentCount = commentCount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public int getSenderSex() {
+        return senderSex;
+    }
+
+    public void setSenderSex(int senderSex) {
+        this.senderSex = senderSex;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public int getRecipientSex() {
+        return recipientSex;
+    }
+
+    public void setRecipientSex(int recipientSex) {
+        this.recipientSex = recipientSex;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getThumbsUp() {
+        return thumbsUp;
+    }
+
+    public void setThumbsUp(int thumbsUp) {
+        this.thumbsUp = thumbsUp;
+    }
+
+    public int getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(int commentCount) {
+        this.commentCount = commentCount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return id == post.id && senderSex == post.senderSex && recipientSex == post.recipientSex && thumbsUp == post.thumbsUp && commentCount == post.commentCount && Objects.equals(sender, post.sender) && Objects.equals(recipient, post.recipient) && Objects.equals(createTime, post.createTime) && Objects.equals(content, post.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, sender, senderSex, recipient, recipientSex, createTime, content, thumbsUp, commentCount);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", sender='" + sender + '\'' +
+                ", senderSex=" + senderSex +
+                ", recipient='" + recipient + '\'' +
+                ", recipientSex=" + recipientSex +
+                ", createTime=" + createTime +
+                ", content='" + content + '\'' +
+                ", thumbsUp=" + thumbsUp +
+                ", commentCount=" + commentCount +
+                '}';
+    }
 }
